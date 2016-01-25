ea2252f35568c56a59c04bef321b09be1b0bd71b
Wiebe van Breukelen <wvanbreukelen@users.noreply.github.com>
2016-01-25 21:52:54 +0100

    Changed if position

--- www/lib/Zermelo/ZermeloHelper.php
+++ www/lib/Zermelo/ZermeloHelper.php
@@ -335,28 +335,23 @@ class ZermeloHelper
 			     }
 			     
 			  
-			} else {
-				
-				
-				if ($node['start'] == $before)
-				{
-					
-					print_r($node);
-			     
-				     	if ($node['cancelled'] == true)
-				     	{
-				     	  	
-				     	  	
-				     	 	//echo "Ik heb dit uur weggehaald! Cancelled staat op true, ook is de starttijd " . $node['start'] . " gelijk aan het vorige uur, " . $before . "!";
-				     	 	unset($grid[$key]);
-				     	} else {
-				     		$timestamps[$key] = $node['start'];
-				     	}
-				} else {
-			    		$timestamps[$key] = $node['start'];	
-				}
 			}
 			
+			if ($node['start'] == $before)
+			{
+					
+				print_r($node);
+		     
+			     	if ($node['cancelled'] == true)
+			     	{
+			     	 	//echo "Ik heb dit uur weggehaald! Cancelled staat op true, ook is de starttijd " . $node['start'] . " gelijk aan het vorige uur, " . $before . "!";
+			     	 	unset($grid[$key]);
+			     	} else {
+			     		$timestamps[$key] = $node['start'];
+			     	}
+			} else {
+		    		$timestamps[$key] = $node['start'];	
+			}
 
 			
 			$before = $node['start'];
