3cde41b7f80f61c32cd1bd27329bc616b817975b
Server <server@matthijsotterloo.com>
2016-01-27 18:14:42 +0100

    Server commit

--- www/lib/Zermelo/Handler.php
+++ www/lib/Zermelo/Handler.php
@@ -126,7 +126,7 @@ class Handler implements \Core\Handler {
 	        $item = (object)$item;
 	        $start = ((int)$item->start);
                 $vakname = isset($subjects[$item->subjects[0]]) ? $subjects[$item->subjects[0]] : $item->subjects[0];
-                $teacher = isset($item->teachers[0]) ? $item->teachers[0] : $item->teachers;
+                $teacher = isset($item->teachers[0]) ? $item->teachers[0] : "onbekend";
                 $cancelled = $item->cancelled;
                 $moved  = $item->moved;
                 $cancelled = $item->cancelled;
