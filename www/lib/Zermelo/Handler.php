45cb18f7770a303cfc75f0223ecd2dc8e26931ac
Matthijs Otterloo <matthijsotterloo@users.noreply.github.com>
2016-02-06 09:21:03 +0100

    Trying to set changed teacher.

--- www/lib/Zermelo/Handler.php
+++ www/lib/Zermelo/Handler.php
@@ -142,6 +142,13 @@ class Handler implements \Core\Handler {
 					$item->locations = array('onbekend');
 				}
 				
+				$explodeTeacher = explode("de les is opgevangen door ", $changedDesc);
+				if (isset($explodeTeacher[1]))
+				{
+					$changedTeacher = explode(".", $explodeTeacher[1]);
+					$item->teachers[0] = str_replace(" ", "", $changedTeacher[0]);
+				}
+				
 				$explode = explode("gewijzigd naar ", $changedDesc);
 				
 				if (isset($explode[1]))
